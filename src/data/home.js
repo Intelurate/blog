15c0ad9e7f163b3bd553f5897e80f189dff9d3f1
BearLikeCode <bearlikecode@gmail.com>
2020-01-24 10:31:12 +0200

    Remove lines with author and array rising from home.js

--- src/data/home.js
+++ src/data/home.js
@@ -4,7 +4,6 @@ module.exports = {
           theme: "World",
           title: "U.S. marshal killed serving warrant at mobile",
           bgImage: "../../assets/images/post/slider1.jpg",
-          author: "Adam Hianks",
           share: "643",
           time: "9 Hour ago",
           description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ex ea commodo consequat."
@@ -13,7 +12,6 @@ module.exports = {
           theme: "Technology",
           title: "Chelsea Handler slams Angelina yet again",
           bgImage: "./assets/images/post/slider3.jpg\"",
-          author: "Matt Cloey",
           share: "218",
           time: "5 Hour ago",
           description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ex ea commodo consequat."
@@ -22,7 +20,6 @@ module.exports = {
           theme: "Business",
           title: "Mirum est notare quam littera gothica",
           bgImage: "../../../assets/images/post/slider4.jpg",
-          author: "Jhon Dun",
           share: "512",
           time: "3 Hour ago",
           description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ex ea commodo consequat."
@@ -31,41 +28,9 @@ module.exports = {
           theme: "Politics",
           title: "Duis autem vel eum iriure dolor in hendrerit in",
           bgImage: "../../../../assets/images/post/slider5.jpg",
-          author: "Kolony Wispe",
           share: "610",
           time: "7 Hour ago",
           description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ex ea commodo consequat."
       },
-  ],
-    rising: [
-        {
-            img: "images/post/9.jpg",
-            theme: "Entertainment",
-            title: "Howard Stern: 'I told Trump' I'm voting for Clinton",
-            detailsPost: "details1.html",
-            detailsTheme: "#"
-        },
-        {
-            img: "images/post/10.jpg",
-            theme: "Business",
-            title: "Our closest relatives aren't fans of daylight saving time",
-            detailsPost: "details1.html",
-            detailsTheme: "#"
-        },
-        {
-            img: "images/post/11.jpg",
-            theme: "Technology",
-            title: "And the most streamed Beatles song on Spotify is...",
-            detailsPost: "details1.html",
-            detailsTheme: "#"
-        },
-        {
-            img: "images/post/12.jpg",
-            theme: "Sports",
-            title: "Our closest relatives aren't fans of daylight saving time",
-            detailsPost: "details1.html",
-            detailsTheme: "#"
-        }
-    ]
-
+  ]
 };
